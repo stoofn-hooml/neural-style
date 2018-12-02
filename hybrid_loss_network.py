fix normalization, continue temporal loss implementation

hybrid_loss_network.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+# from warp import warp_image
+
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -33,19 +35,17 @@ class Normalization(nn.Module):
 class ContentLoss(nn.Module):
     """ computes loss between content image and generated using MSE for spatial loss"""
 
-    def __init__(self, gpu):
+    def __init__(self, device):
         super(ContentLoss, self).__init__()
-        if gpu:
-            loss = nn.MSELoss().cuda()
-        else:
-            loss = nn.MSELoss()
-        self.loss = loss
+        self.loss = nn.MSELoss().to(device)
 
     def forward(self, content_activation, generated_activation):
         # inputs are the respective feature maps at ReLU_10 (final loss layer)
         b, c, h, w = content_activation.shape
+        return (1/(c*h*w) * self.loss(content_activation, generated_activation))
 
-        return (1 /(c * h * w)) * torch.mean((content_activation - generated_activation) ** 2)
+        # why not use MSE?
+        # return (1 /(c * h * w)) * torch.mean((content_activation - generated_activation) ** 2)
 
 def gram_matrix(input):
     a, b, c, d = input.size()  # a=batch size(=1)
@@ -63,20 +63,18 @@ def gram_matrix(input):
 class StyleLoss(nn.Module):
     """ computes loss between style image and generated image using Gram matrix for spatial loss """
 
-    def __init__(self, gpu):
+    def __init__(self, device):
         super(StyleLoss, self).__init__()
-        if gpu:
-            loss = nn.MSELoss().cuda()
-        else:
-            loss = nn.MSELoss()
-        self.loss = loss
+        self.loss = nn.MSELoss().to(device)
 
     def forward(self, style_activation, generated_activation):
         style_gram = gram_matrix(style_activation)
         generated_gram = gram_matrix(generated_activation)
 
         num_channels = generated_activation.shape[3]
-        return (1 / num_channels ** 2) * self.loss(generated_gram, style_gram)
+        # print("Num_channels", num_channels)
+        # print(style_gram, generated_gram)
+        return (1 / num_channels ** 2) * self.loss(style_gram, generated_gram)
 
 class TVLoss(nn.Module):
     """ TV regularization factor for spatial loss (Huang section 3.2.1) """
@@ -98,16 +96,25 @@ class TemporalLoss(nn.Module):
     """
     def __init__(self, device):
         super(TemporalLoss, self).__init__()
-        self.device = device
+        # self.device = device
+        self.loss = nn.MSELoss().to(device)
 
-    def forward(self, generated_t, flow_t1, mask):
-        assert generated_t.shape == flow_t1, "inputs are not the same"
-        generated_t = generated_t.view(1, -1).to(device)
-        flow_t1 = flow_t1.view(1, -1).to(device)
-        mask = mask.view(-1).to(device)
+    def forward(self, generated_t, generated_t1, flow_t1, mask):
+        print(generated_t.shape, flow_t1.shape, mask.shape)
+        # assert generated_t.shape == flow_t1.shape, "inputs are not the same"
+        # generated_t_reshaped = generated_t.squeeze(0).permute(1, 2, 0)
+        # flow_t1 = flow_t1.view(1, -1)
+        # mask = mask.view(-1)
 
-        D = flow_t1.shape[1]
-        return (1 / D) * mask * generated_t, flow_t1
+        # .squeeze(0).permute(1, 2, 0)
+
+        print(generated_t_reshaped.shape, flow_t1.shape)
+
+        b, c, h, w = generated_t.shape
+        D = h * w * c
+        warped = nn.grid_sample(generated_t1, )
+        # D = flow_t1.shape[1]
+        return ((1 / D) * mask * self.loss(generated_t, warp_image(generated_t1, flow_t1)))
 
 
 """ Defaults for hybrid loss network """

opticalflow.py
@@ -5,22 +5,26 @@ import numpy as np
 
 # input is numpy image array
 def opticalflow(generated_t, generated_t1):
-    print(generated_t.shape, generated_t1.shape)
+    # print(generated_t.shape, generated_t1.shape)
 
     generated_t_gray = cv2.cvtColor(generated_t, cv2.COLOR_RGB2GRAY)
     generated_t1_gray = cv2.cvtColor(generated_t1, cv2.COLOR_RGB2GRAY)
-    hsv = np.zeros_like(generated_t_gray)
-    hsv[..., 1] = 255
 
     flow = cv2.calcOpticalFlowFarneback(generated_t_gray, generated_t1_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    # print(ang.shape)
+
+    hsv = np.zeros_like(generated_t)
+    hsv[..., 0] = ang*180/np.pi/2
+    hsv[..., 1] = 255
+    hsv[..., 2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
     gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+    mask = np.where(gray > 0.5, 0, 1)
 
-    mask = np.where(x > 0.5, 0, 1)
-    gray[mask]
+    # cv2.imwrite('flow.png', rgb)
+    # cv2.imwrite('mask.png', mask)
 
-    return rgb, gray[mask]
+    return rgb, mask

train_stylization_network.py
@@ -69,18 +69,23 @@ print(img_shape)
 
 transform = transforms.Compose([
     transforms.Resize(img_shape),  # scale imported image
-    transforms.ToTensor()])  # transform it into a torch tensor
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])  # transform it into a torch tensor
+
+# normalizeImg = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
 # resize, convert to tensor, add dimension, put on GPU if available
-def transformImg(image, style):
+def transformImg(image, style=True, normalize=True):
     # fake batch dimension required to fit network's input dimensions
     image = transform(image) # adds another dimension to tensor
     if (style):
         image = image.unsqueeze(0) # adds another dimension to style tensor
+    # if (normalize):
+    #     image = normalizeImg(image)
     return image.to(device, torch.float)
 
 
-style_img = transformImg(Image.open("./images/starry.jpg"), True)
+style_img = transformImg(Image.open("./images/starry.jpg"), style=True, normalize=True)
 content_path = './content_videos'
 unloader = transforms.ToPILImage()  # reconvert into PIL image
 
@@ -94,7 +99,11 @@ def imshow(tensor, title=None):
     plt.pause(0.001)  # pause a bit so that plots are update
 
 
-model_path = "./models/model" + str(datetime.datetime.now()) + ".pth"
+model_save_path = "./models/model" + str(datetime.datetime.now()).strip('/') + ".pth"
+model_load_path = "./models/model.pth"
+load_model = True
+
+use_temporal_loss = True
 
 # hyperperameter defaults (specified in section 4.1)
 default_content_weight = 1
@@ -102,14 +111,16 @@ default_style_weight = 10
 default_temporal_weight = 10000
 default_variation_weight = .001
 
+default_epochs = 2
+
 # style_loss_layers = [1]
 
 def saveModel(stylization_network):
-    print("Saving model to:", model_path)
-    torch.save(stylization_network.state_dict(), model_path)
+    print("Saving model to:", model_save_path)
+    torch.save(stylization_network.state_dict(), model_save_path)
 
 
-def train_stylization_network(style_img, num_steps=200,
+def train_stylization_network(style_img, epochs=default_epochs,
                        content_weight=default_content_weight,
                        style_weight=default_style_weight,
                        temporal_weight=default_temporal_weight,
@@ -119,13 +130,18 @@ def train_stylization_network(style_img, num_steps=200,
 
     # initialize starting values/networks
     stylization_network = StylizationNetwork().to(device)
+    if (load_model):
+        print("Loading: ", model_load_path)
+        stylization_network.load_state_dict(torch.load(model_load_path))
+
     spatial_loss_network = SpatialLossNetwork(device).to(device)
 
     content_loss = ContentLoss(device).to(device)
     style_loss = StyleLoss(device).to(device)
     tv = TVLoss(device).to(device)
-    # temporal_loss = TemporalLoss(device).to(device)
+    temporal = TemporalLoss(device).to(device)
 
+    # using default learning rate 0.001
     optimizer = optim.Adam(stylization_network.parameters())
 
     print('Optimizing...')
@@ -134,13 +150,16 @@ def train_stylization_network(style_img, num_steps=200,
     # get loader of video
     video_loader = get_loader(1, content_path, transformImg)
 
-    while steps_completed <= num_steps:
+    for epoch in range(epochs):
+        print("Epoch: ", epoch)
+
         # video_loader is an iterator so it must be accessed with enumerate()
         # each element of enumerate(video_loader) is a list of frames (a video)
-        for _, frames in enumerate(video_loader):
+        for j, frames in enumerate(video_loader):
+            print("Video", j)
             # loop through all the frames for each video
             for i in range(1, len(frames)):
-                print("Video", i)
+                # print("Frame", i)
 
                 content_t = frames[i];      # current frame
                 content_t1 = frames[i-1];   # previous frame
@@ -148,6 +167,7 @@ def train_stylization_network(style_img, num_steps=200,
                 generated_t = stylization_network(content_t).to(device)
                 generated_t1 = stylization_network(content_t1).to(device)
 
+                # don't clamp! Then it's not the output of the stylization network being optimized
                 # generated_t.data.clamp_(0, 1)    # should we clamp? Tut does, example doesn't
                 # generated_t1.data.clamp_(0, 1)
 
@@ -185,25 +205,19 @@ def train_stylization_network(style_img, num_steps=200,
                 # final spatial loss
                 spatial_loss = total_style_loss + total_content_loss + tv_loss
 
-                # from deepmatching import deepmatching
-                # from deepflow2 import deepflow2
-                # import numpy
-                #
-                # im1 = numpy.array(Image.open('sintel1.png'))
-                # im2 = numpy.array(Image.open('sintel2.png'))
-                # matches = deepmatching(im1, im2)
-                # flow = deepflow2(im1, im2, matches, '-sintel')
-
                 # Optical flow (Temporal Loss, section 3.2.2)
-                # flow_t1, mask = opticalflow(generated_t.squeeze(0).permute(1, 2, 0).data.numpy(), generated_t1.squeeze(0).permute(1, 2, 0).data.numpy())
-                #
-                # temporal_loss = TemporalLoss(generated_t, flow_t1, mask)
-                # temporal_loss *= temporal_weight
+                # .squeeze(0).permute(1, 2, 0)
+                if use_temporal_loss:
+                    optical_t = generated_t.squeeze(0).permute(1, 2, 0).cpu().data.numpy()
+                    optical_t1 = generated_t1.squeeze(0).permute(1, 2, 0).cpu().data.numpy()
+                    flow_t1, mask = opticalflow(optical_t, optical_t1)
 
-                # Hybrid loss and backprop
-                # hybrid_loss = spatial_loss + temporal_loss
+                    temporal_loss = temporal(generated_t, generated_t1, flow_t1, mask)
+                    temporal_loss *= temporal_weight
 
-                hybrid_loss = spatial_loss
+                    hybrid_loss = spatial_loss + temporal_loss
+                else:
+                    hybrid_loss = spatial_loss
 
                 # clears gradients for each iteration of backprop
                 optimizer.zero_grad()
@@ -216,8 +230,8 @@ def train_stylization_network(style_img, num_steps=200,
 
                 steps_completed += 1
                 if steps_completed % 50 == 0:
-                    print("Step {}:".format(steps_completed))
-                    print('Style Loss : {:4f} Content Loss: {:6f} TV Loss: {:4f}'.format(
+                    print("Frames completed {}:".format(steps_completed))
+                    print('Hybrid loss: {:4f}\nStyle Loss : {:4f} Content Loss: {:8f} TV Loss: {:4f}'.format(hybrid_loss.item(),
                         total_style_loss.item(), total_content_loss.item(), tv_loss.item()))
                     print()
 