758c7be9e7d51a1724ab7fcd1aa1f06b0af9e94c
Kyle Meredith <Kyle@kyels-copmuter.middlebury.edu>
2018-12-02 16:10:00 -0500

    working stylization

--- stylize_video.py
+++ stylize_video.py
@@ -1,30 +1,53 @@
 import cv2
 from stylization_network import StylizationNetwork
 from train_stylization_network import transformImg, imshow
+import torchvision.transforms as transforms
 import torch
 from PIL import Image
+import re
 
 import matplotlib.pyplot as plt
+
+from torch.utils.serialization import load_lua
+
+from transformer_net import TransformerNet
 
 video_path = './videos_to_stylize/fulldogs.mov'
 
 video = cv2.VideoCapture(video_path)
 
 # https://stackoverflow.com/questions/42703500/best-way-to-save-a-trained-model-in-pytorch
-model_path = "./models/model.pth"
-stylization_network = StylizationNetwork()
-stylization_network.load_state_dict(torch.load(model_path))
+# model_path = "./models/model.pth"
+# stylization_network = StylizationNetwork()
+# stylization_network.load_state_dict(torch.load(model_path, map_location='cpu'))
+# stylization_network.eval()
+
+# stylization_network = load_lua('./models/starry_night.t7')
+
+stylization_network = TransformerNet()
+state_dict = torch.load('./models/mosaic.pth')
+# remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+for k in list(state_dict.keys()):
+    if re.search(r'in\d+\.running_(mean|var)$', k):
+        del state_dict[k]
+stylization_network.load_state_dict(state_dict)
+
+# stylization_network = torch.load('./models/starry_night.t7')
 
 fps = video.get(cv2.CAP_PROP_FPS)
 print(fps)
 wait_time = int(1/fps*360)
-# wait_time = 1
-print(wait_time)
 
 if (not video.isOpened()):
     print("Error loading ", video_path)
 
 frame_count = 0
+
+content_transform = transforms.Compose([
+    transforms.Resize((240, 426)),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.mul(255))
+])
 
 while(True):
     ret, frame = video.read()
@@ -33,20 +56,55 @@ while(True):
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     pil_frame = Image.fromarray(rgb_frame)
 
+    # print(pil_frame)
+    transformed_frame = content_transform(pil_frame)
+    transformed_frame = transformed_frame.unsqueeze(0)
+
     # resize, convert to tensor, add dimension, put on GPU if available
     # rgb_frame = self.transformImg(rgb_frame)
-    transformed_frame = transformImg(pil_frame, True)
+    # transformed_frame = transformImg(pil_frame, True, True)
 
-    stylized_frame = stylization_network(transformed_frame)
-    stylized_frame.data.clamp_(0, 1)
+    output = stylization_network(transformed_frame).cpu()
+    # stylized_frame.data.clamp_(0, 1)
 
-    print(stylized_frame, stylized_frame.shape)
+    # print(stylized_frame, stylized_frame.shape)
+    # [1, 3, 240, 428]
+    stylized_frame = output[0].detach().clamp(0, 255).numpy()
+    stylized_frame = stylized_frame.transpose(1, 2, 0).astype("uint8")
+    # stylized_frame = Image.fromarray(stylized_frame)
+
+    # mean: (0.485, 0.456, 0.406), std: (0.229, 0.224, 0.225)
+
+    # stylized_frame[0] = stylized_frame[0]*0.229 + 0.485
+    # stylized_frame[1] = stylized_frame[1]*0.224 + 0.456
+    # stylized_frame[2] = stylized_frame[2]*0.225 + 0.406
+
+    # stylized_frame[0] = stylized_frame[0]+ 0.485
+    # stylized_frame[1] = stylized_frame[1] + 0.456
+    # stylized_frame[2] = stylized_frame[2] + 0.406
+    # stylized_frame *= 255
+
+    # stylized_frame.data.clamp_(0, 1)
+
+    # stylized_frame = stylized_frame.permute(1, 2, 0).cpu().data.numpy()
+    # print(stylized_frame, stylized_frame.shape)
+    # [240, 428, 3]
+
+    cv2.imshow("Stylized", stylized_frame)
+    # stylized_frame = stylized_frame.reshape((3, output.shape[2], output.shape[3]))
+	# stylized_frame[0] += 103.939
+	# stylized_frame[1] += 116.779
+	# stylized_frame[2] += 123.680
+	# stylized_frame /= 255.0
+	# stylized_frame = stylized_frame.transpose(1, 2, 0)
+
+    # print(stylized_frame, stylized_frame.shape)
 
     # plt.figure()
-    imshow(stylized_frame, title='Stylized')
+    # imshow(stylized_frame, title='Stylized')
 
     # plt.figure()
-    imshow(transformed_frame, title='Normal')
+    # imshow(transformed_frame, title='Normal')
 
     # cv2.imshow('Frame', frame)
 
